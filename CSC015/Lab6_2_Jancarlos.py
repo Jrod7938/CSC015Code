bdcd6871284b54daab25589347320fa10cf89589
jc <jrod7938@gmail.com>
2021-11-03 15:31:24 -0400

    Completed Lab6 Q2

--- CSC015/Lab6_2_Jancarlos.py
+++ CSC015/Lab6_2_Jancarlos.py
@@ -1,5 +1,31 @@
 #Jancarlos Rodriguez
 #Lab6 Q2
 #Lab6_2_Jancarlos.py
-#Program gives information, formula, and value of the Golden Ratio
+#The program prints a table of the integer powers of 10 from 10􏰍􏰎 𝑡𝑜 10􏰎
 #20211103
+
+def intpower(x, k):
+    value = x
+    if k > 0:
+        for i in range(int(k - 1)):
+            value *= x
+            i += 1
+
+    elif k == 0:
+        value = 1
+    else:
+        x = 1 / x
+        for i in range(abs(k - 1)):
+            value *= x
+            i += 1
+    return value
+
+def main():
+
+    print(" k      10^k")
+    print("---  -----------")
+    for i in range(-5,6):
+        print(f"{i}   {intpower(10, i)}")
+        i += 1
+
+main()
